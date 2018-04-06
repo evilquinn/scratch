273410a9af173d9b8edf4edd6215c668fc8f3953
evilquinn <evilquinn@gmail.com>
2018-04-07 00:46:11 +0100

    minor hex tidy

--- hexscratch.cpp
+++ hexscratch.cpp
@@ -6,13 +6,12 @@
 int main()
 {
     std::string hexed("4142434445464748");
-    std::string unhexed;
 
     // print it
-    std::cout << hexed << std::endl;
     boost::algorithm::unhex(hexed, std::ostream_iterator<char>(std::cout));
     std::cout << std::endl;
     // or save it
+    std::string unhexed;
     boost::algorithm::unhex(hexed, std::back_inserter(unhexed));
     std::cout << unhexed << std::endl;
     return 0;
